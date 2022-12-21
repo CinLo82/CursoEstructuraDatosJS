b1320d0603420e93465a9ef8d23c5eea60a29b49
CinLo <cinlos3382@gmail.com>
2022-12-21 12:14:16 -0300

    metodo append(agrega elem en la cola de la lista); metodo prepend(agrega elem en la cabeza de la lista)

--- linkedList/singly.js
+++ linkedList/singly.js
@@ -1,20 +1,20 @@
-1 --> 2 --> 3 --> 4 --> null
+// 1 --> 2 --> 3 --> 4 --> null
 
-let sunglyLinkedList = {
-    head: {
-        value:1,
-        next: {
-            value:2,
-            next: {
-                value:3,
-                next: {
-                    value:4,
-                    next: null
-                }
-            }
-        }
-    }
-}
+// let sunglyLinkedList = {
+//     head: {
+//         value:1,
+//         next: {
+//             value:2,
+//             next: {
+//                 value:3,
+//                 next: {
+//                     value:4,
+//                     next: null
+//                 }
+//             }
+//         }
+//     }
+// }
 
 class Node {
     constructor(value) {
@@ -32,6 +32,24 @@ class MySinglyLinkedList {
         this.tail = this.head;
 
         this.lenght = 1;
+    }
+    append(value) {
+        const newNode = new Node(value);
+
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.lenght++;
+
+        return this;
+    }
+    prepend(value) {
+        const newNode = new Node(value);
+
+        newNode.next = this.head;
+        this.head = newNode;
+        this.lenght++;
+
+        return this;
     }
 }
 
